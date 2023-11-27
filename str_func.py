536416c50a98e4e506e987139b80624f6d4f5214
katerina <katyushka555@yandex.ru>
2023-11-28 00:38:16 +0400

    add str_func.py

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def capital_letters(words: str) -> str:
+    """
+    Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами
+    :param words:
+    :return:
+    """
+    return words.upper()
+
